ac1d6514bc1a4924196a9ad30c40fe83fd5acad8
ntibi <ntibi@protonmail.com>
2025-11-16 22:16:11 +0100

    fix fan color output

--- waybar/custom_modules/fan.sh
+++ waybar/custom_modules/fan.sh
@@ -3,22 +3,14 @@
 level=$(awk '/^level:/{print $2}' /proc/acpi/ibm/fan)
 speed=$(awk '/^speed:/{print $2}' /proc/acpi/ibm/fan)
 
-if [ "$level" = "auto" ]; then
-	class="auto"
-elif [ "$level" = "disengaged" ] || [ "$level" = "full-speed" ]; then
-	class="full"
-else
-	class="manual"
-fi
-
 if [ "$speed" -lt 3000 ]; then
-	speed_class="low"
+	class="low"
 elif [ "$speed" -lt 4500 ]; then
-	speed_class="medium"
+	class="medium"
 elif [ "$speed" -lt 5500 ]; then
-	speed_class="high"
+	class="high"
 else
-	speed_class="full"
+	class="full"
 fi
 
-echo "{\"text\": \"$speed\", \"class\": \"$class $speed_class\", \"tooltip\": \"fan: $level\"}"
+echo "{\"text\": \"$speed\", \"class\": \"$class\", \"tooltip\": \"level: $level\"}"

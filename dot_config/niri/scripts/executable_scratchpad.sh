7ec332b5b80cb5d1d07fd0279f0c2a1501286e8a
ntibi <ntibi@protonmail.com>
2025-12-24 16:38:10 +0100

    fix move to workspace scratchpad

--- dot_config/niri/scripts/executable_scratchpad.sh
+++ dot_config/niri/scripts/executable_scratchpad.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 app_id="scratchpad"
+magic_workspace="magic"
 
 if niri msg --json focused-window | jq -r .app_id | grep "^$app_id$"; then
-    niri msg action close-window
+    # send to magic workspace
+    niri msg action move-window-to-workspace $magic_workspace --focus false
 elif niri msg --json windows | jq -r ".[] | .app_id" | grep "^$app_id$"; then
-    niri msg action close-window --id "$(niri msg --json windows | jq -r ".[] | select(.app_id==\"$app_id\") | .id")"
-    niri msg action spawn -- alacritty --class "$app_id" -e vi ~/TODO
+    # bring back from wherever
+    id=$(niri msg --json windows | jq -r ".[] | select(.app_id==\"$app_id\") | .id")
+    workspace=$(niri msg --json workspaces | jq '.[] | select(.is_focused==true) | .id')
+    niri msg action move-window-to-workspace --window-id "$id" --focus true "$workspace"
+    niri msg action focus-window --id "$id"
 else
+    # spawn new
     niri msg action spawn -- alacritty --class "$app_id" -e vi ~/TODO
 fi
